de247451db83eac9522f36ddb66a777bb03bab53
Daniel Arias <ingdsag@hotmail.com>
2016-09-21 23:41:39 +0200

    Adding last details

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -2,12 +2,12 @@ var gulp = require( 'gulp' );
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
 
     var sources = [
         './bower_components/jquery/dist/jquery.js',
-        './bower_components/bootstrap.min.js',
         './bower_components/angular/angular.js',
         './bower_components/angular-route/angular-route.js'
 
@@ -17,6 +17,7 @@ gulp.task('scripts', function() {
         .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(concat('lib.js'))
+        .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/app'));
     return stream;
